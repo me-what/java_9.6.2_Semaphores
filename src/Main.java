2ce154f32cdaa0ff87585d1a56ffad76c61b1648
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-07-09 17:19:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.Semaphore;
+
+public class Main {
+    public static void main(String[] args) {
+        Semaphore sem = new Semaphore(2);
+        for(int i=1;i<6;i++)
+            new Philosopher(sem,i).start();
+    }
+}
